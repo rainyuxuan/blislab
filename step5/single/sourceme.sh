02b56ab6979a49ce3151f56a96d95fa802a8ed74
rainyuxuan <yu2001xuan@outlook.com>
2023-02-22 02:50:18 -0500

    Fixed sourceme.sh

--- step5/single/sourceme.sh
+++ step5/single/sourceme.sh
@@ -6,17 +6,17 @@ echo "BLISLAB_DIR = $BLISLAB_DIR"
 export BLISLAB_USE_INTEL=true
 echo "BLISLAB_USE_INTEL = $BLISLAB_USE_INTEL"
 
-# Whether use BLAS or not?
-export BLISLAB_USE_BLAS=true
+# Whether reference implementation uses BLAS or not?
+export BLISLAB_USE_BLAS=false
 echo "BLISLAB_USE_BLAS = $BLISLAB_USE_BLAS"
 
 # Optimization Level (O0, O1, O2, O3)
-export COMPILER_OPT_LEVEL=O3
+export COMPILER_OPT_LEVEL=O0
 echo "COMPILER_OPT_LEVEL = $COMPILER_OPT_LEVEL"
 
 # Manually set the BLAS path if BLIS_USE_BLAS=true and using GNU compiler.
 #export BLAS_DIR=/u/jianyu/lib/blis
-export BLAS_DIR=/u/jianyu/lib/openblas
+export BLAS_DIR=/opt/intel/mkl-lib
 echo "BLAS_DIR = $BLAS_DIR"
 
 # Parallel Options
