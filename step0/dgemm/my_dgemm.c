0fd555512cf28341496a922debc967b048613cec
rainyuxuan <yu2001xuan@outlook.com>
2023-02-22 00:33:39 -0500

    Format file

--- step0/dgemm/my_dgemm.c
+++ step0/dgemm/my_dgemm.c
@@ -42,41 +42,40 @@
  *
  * 
  * */
- 
+
 
 #include "bl_dgemm.h"
 
 
 void bl_dgemm(
-    int    m,
-    int    n,
-    int    k,
-    double *A,
-    int    lda,
-    double *B,
-    int    ldb,
-    double *C,        // must be aligned
-    int    ldc        // ldc must also be aligned
-)
-{
-  int    i, j, p;
+        int m,
+        int n,
+        int k,
+        double *A,
+        int lda,
+        double *B,
+        int ldb,
+        double *C,        // must be aligned
+        int ldc        // ldc must also be aligned
+) {
+    int i, j, p;
 
-  // Early return if possible
-  if ( m == 0 || n == 0 || k == 0 ) {
-    printf( "bl_dgemm(): early return\n" );
-    return;
-  }
+    // Early return if possible
+    if (m == 0 || n == 0 || k == 0) {
+        printf("bl_dgemm(): early return\n");
+        return;
+    }
 
-  for ( j = 0; j < n; j ++ ) {              // Start 2-nd loop
-      for ( p = 0; p < k; p ++ ) {          // Start 1-st loop
-          for ( i = 0; i < m; i ++ ) {      // Start 0-th loop
+    for (j = 0; j < n; j++) {              // Start 2-nd loop
+        for (p = 0; p < k; p++) {          // Start 1-st loop
+            for (i = 0; i < m; i++) {      // Start 0-th loop
 
-              //C[ j * ldc + i ] += A[ p * lda + i ] * B[ j * ldb + p ];
-              C( i, j ) += A( i, p ) * B( p, j ); //Each operand is a MACRO defined in bl_dgemm() function.
+                //C[ j * ldc + i ] += A[ p * lda + i ] * B[ j * ldb + p ];
+                C(i, j) += A(i, p) * B(p, j); //Each operand is a MACRO defined in bl_dgemm() function.
 
-          }                                 // End   0-th loop
-      }                                     // End   1-st loop
-  }                                         // End   2-nd loop
+            }                                 // End   0-th loop
+        }                                     // End   1-st loop
+    }                                         // End   2-nd loop
 
 }
 
--- step1/dgemm/my_dgemm.c
+++ step1/dgemm/my_dgemm.c
@@ -42,56 +42,54 @@
  *
  * 
  * */
- 
+
 
 #include "bl_dgemm.h"
 
-void AddDot( int k, double *A, int lda, double *B, int ldb, double *result ) {
-  int p;
-  for ( p = 0; p < k; p++ ) {
-    *result += A( 0, p ) * B( p, 0 );
-  }
+void AddDot(int k, double *A, int lda, double *B, int ldb, double *result) {
+    int p;
+    for (p = 0; p < k; p++) {
+        *result += A(0, p) * B(p, 0);
+    }
 }
 
 
-void AddDot_MRxNR( int k, double *A, int lda, double *B, int ldb, double *C, int ldc )
-{
-  int ir, jr;
-  int p;
-  for ( jr = 0; jr < DGEMM_NR; jr++ ) {
-    for ( ir = 0; ir < DGEMM_MR; ir++ ) {
+void AddDot_MRxNR(int k, double *A, int lda, double *B, int ldb, double *C, int ldc) {
+    int ir, jr;
+    int p;
+    for (jr = 0; jr < DGEMM_NR; jr++) {
+        for (ir = 0; ir < DGEMM_MR; ir++) {
 
-      AddDot( k, &A( ir, 0 ), lda, &B( 0, jr ), ldb, &C( ir, jr ) );
+            AddDot(k, &A(ir, 0), lda, &B(0, jr), ldb, &C(ir, jr));
 
+        }
     }
-  }
 }
 
 void bl_dgemm(
-    int    m,
-    int    n,
-    int    k,
-    double *A,
-    int    lda,
-    double *B,
-    int    ldb,
-    double *C,        // must be aligned
-    int    ldc        // ldc must also be aligned
-)
-{
-    int    i, j, p;
-    int    ir, jr;
+        int m,
+        int n,
+        int k,
+        double *A,
+        int lda,
+        double *B,
+        int ldb,
+        double *C,        // must be aligned
+        int ldc        // ldc must also be aligned
+) {
+    int i, j, p;
+    int ir, jr;
 
     // Early return if possible
-    if ( m == 0 || n == 0 || k == 0 ) {
-        printf( "bl_dgemm(): early return\n" );
+    if (m == 0 || n == 0 || k == 0) {
+        printf("bl_dgemm(): early return\n");
         return;
     }
 
-    for ( j = 0; j < n; j += DGEMM_NR ) {          // Start 2-nd loop
-        for ( i = 0; i < m; i += DGEMM_MR ) {      // Start 1-st loop
+    for (j = 0; j < n; j += DGEMM_NR) {          // Start 2-nd loop
+        for (i = 0; i < m; i += DGEMM_MR) {      // Start 1-st loop
 
-            AddDot_MRxNR( k, &A( i, 0 ), lda, &B( 0, j ), ldb, &C( i, j ), ldc );
+            AddDot_MRxNR(k, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
 
         }                                          // End   1-st loop
     }                                              // End   2-nd loop
